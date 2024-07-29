98f165e80db40f837e21df4fd52e7de9a3080f6b
Piotr Pośpiech <piootrp01@gmail.com>
2024-07-30 00:34:48 +0200

    fix .env file

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,18 +1,24 @@
+require('dotenv/config') 
 const mysql = require('mysql2');
-require('dotenv').config();
+//require('dotenv').config();
 
-// const connection = mysql.createConnection({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_NAME,
-// });
+console.log('DB_HOST:', process.env.DB_HOST); // Debug log
+console.log('DB_USER:', process.env.DB_USER); // Debug log
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD); // Debug log
+console.log('DB_NAME:', process.env.DB_NAME); // Debug log
+
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "password",
-  database: "airport_advisor_system_db",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
+// const connection = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "password",
+//   database: "airport_advisor_system_db",
+// });
 
 connection.connect((err) => {
   if (err) {
