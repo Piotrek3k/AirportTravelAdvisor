869f853129b79a14a0daa24fe7ace22713c4deaa
Piotr Pośpiech <piootrp01@gmail.com>
2024-08-05 16:07:29 +0200

    add register and login - without authorization yet

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,12 +1,35 @@
-const db = require('../../config/dbConfig');
+const db = require('../db');
+
 
 const User = {
     getAll: (callback) => {
-        db.query(`SELECT * FROM users`, callback)
+        db.query('SELECT * FROM users', callback);
     },
-    findPath: () => {
-        
-    }
-}
 
-export default User
+    getById: (id, callback) => {
+        db.query('SELECT * FROM users WHERE id = ?', [id], callback);
+    },
+
+    getByUsername: (username, callback) => {
+        db.query('SELECT * FROM users WHERE username = ?', [username], callback);
+    },
+    
+    create: (user, callback) => {
+        const { username, password, email, full_name } = user;
+        db.query('INSERT INTO users (username, password, email, full_name) VALUES (?, ?, ?, ?)', 
+            [username, password, email, full_name], callback);
+    },
+
+    update: (id, user, callback) => {
+        const { username, password, email, full_name } = user;
+        db.query('UPDATE users SET username = ?, password = ?, email = ?, full_name = ? WHERE id = ?', 
+            [username, password, email, full_name, id], callback);
+    },
+
+    delete: (id, callback) => {
+        db.query('DELETE FROM users WHERE id = ?', [id], callback);
+    },
+
+};
+
+module.exports = User;
