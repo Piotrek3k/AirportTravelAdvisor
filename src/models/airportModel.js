dc9490a39097c77a4359f918c3295c4ac1e66c01
Piotr Pośpiech <piootrp01@gmail.com>
2024-08-05 15:28:16 +0200

    fix airport model

--- src/models/airportModel.js
+++ src/models/airportModel.js
@@ -3,6 +3,23 @@ const db = require('../../config/dbConfig');
 const Airport = {
     getAll: (callback) => {
         db.query(`SELECT * FROM airports`, callback)
+    },
+    getById: (id, callback) => {
+        db.query('SELECT * FROM airports WHERE id = ?', [id], callback);
+    },
+
+    create: (airport, callback) => {
+        const query = 'INSERT INTO airports (code, name, city, country) VALUES (?, ?, ?, ?)';
+        db.query(query, [airport.code, airport.name, airport.city, airport.country], callback);
+    },
+
+    update: (id, airport, callback) => {
+        const query = 'UPDATE airports SET code = ?, name = ?, city = ?, country = ? WHERE id = ?';
+        db.query(query, [airport.code, airport.name, airport.city, airport.country, id], callback);
+    },
+
+    delete: (id, callback) => {
+        db.query('DELETE FROM airports WHERE id = ?', [id], callback);
     }
 }
 
