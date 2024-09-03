8eecaa997af6281de96e32fbbab3c66b1d15b1e8
Piotr Pośpiech <piootrp01@gmail.com>
2024-09-03 17:49:08 +0200

    clean code in createDb file

--- config/createDb.js
+++ config/createDb.js
@@ -1,7 +1,7 @@
 const connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "password",
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
   });
   
   connection.connect((err) => {
